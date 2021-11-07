422082b7050cc7bef45fee69369756264c37f1bb
e <942680978@qq.com>
2021-11-07 11:47:55 +0800

    测试文章

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -1,28 +1,100 @@
 export default {
-  title: "网站名称",
-  // 格式是: /仓库名称
-  base: "/vitepressDemo",
+  lang: 'en-US',
+  title: 'VueThree',
+  description: 'Vue3.x technical documentation',
+  // base: "/VueCourse",
   markdown: {
     lineNumbers: true
   },
-  // 主题配置
   themeConfig: {
-    // 文档所在的文件夹
     docsDir: 'docs',
-    // 头部nav
+
+    editLinks: true,
+    editLinkText: '在 GitHub 上编辑此页',
+    lastUpdated: '上次更新',
+
+    // custom containers
+    tip: '提示',
+    warning: '注意',
+    danger: '警告',
+    info: "引用",
+
+    // 404 page
+    notFound: [
+      '这里什么都没有',
+      '我们怎么到这来了？',
+      '这是一个 404 页面',
+      '看起来我们进入了错误的链接',
+    ],
+    backToHome: '返回首页',
+
+    algolia: {
+      apiKey: 'c57105e511faa5558547599f120ceeba',
+      indexName: 'vitepress'
+    },
     nav: [
-      { text: '哔哩哔哩', link: "https://space.bilibili.com/36036472" },
-      // ...省略多个
+      { text: 'Guide', link: '/', activeMatch: '^/$|^/guide/' },
+      {
+        text: 'Config Reference',
+        link: '/config/basics',
+        activeMatch: '^/config/'
+      }
     ],
     sidebar: {
-      // 斜杠代表的是docs文件夹
-      '/': [
-        {
-          text: "基础语法", children: [
-            { text: "标题", link: "/document/文件路径不要后缀" }
-          ]
-        }
-      ]
+      '/': getGuideSidebar()
     }
   }
+}
+
+function getGuideSidebar() {
+  return [
+    {
+      text: '基础语法',
+      children: [
+        { text: '1. Vue介绍', link: '/documents/aaa' },
+        { text: '2. Vue基本使用', link: '/documents/bbb' },
+        // { text: '3. MVVM模式', link: '/documents/Vue/mvvm' },
+        // { text: '4. 指令', link: '/documents/Vue/directive' },
+        // { text: '5. 计算属性', link: '/documents/Vue/computed' },
+        // { text: '6. 侦听器', link: '/documents/Vue/watch' }
+      ]
+    },
+    /* {
+       text: 'Vite脚手架',
+       children: [
+         { text: 'Frontmatter', link: '/guide/introduce' },
+         { text: 'Theming', link: '/guide/introduce' },
+         { text: 'API Reference', link: '/guide/introduce' },
+         { text: 'Differences from Vuepress', link: '/guide/introduce' }
+       ]
+     },
+     {
+       text: 'VueRouter',
+       children: [
+         { text: 'Frontmatter', link: '/guide/introduce' },
+         { text: 'Theming', link: '/guide/introduce' },
+         { text: 'API Reference', link: '/guide/introduce' },
+         { text: 'Differences from Vuepress', link: '/guide/introduce' }
+       ]
+     },
+ 
+     {
+       text: 'Vuex',
+       children: [
+         { text: 'Frontmatter', link: '/guide/introduce' },
+         { text: 'Theming', link: '/guide/introduce' },
+         { text: 'API Reference', link: '/guide/introduce' },
+         { text: 'Differences from Vuepress', link: '/guide/introduce' }
+       ]
+     },
+     {
+       text: 'Nuxt',
+       children: [
+         { text: 'Frontmatter', link: '/guide/introduce' },
+         { text: 'Theming', link: '/guide/introduce' },
+         { text: 'API Reference', link: '/guide/introduce' },
+         { text: 'Differences from Vuepress', link: '/guide/introduce' }
+       ]
+     }*/
+  ]
 }
