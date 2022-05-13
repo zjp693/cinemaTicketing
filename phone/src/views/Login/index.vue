a9ffc34eb2534af1ada5691d1762bc1333638280
zjp693 <942680978@qq.com>
2022-05-13 08:22:50 +0800

    update:登录基础样式

--- phone/src/views/Login/index.vue
+++ phone/src/views/Login/index.vue
@@ -1,187 +1,369 @@
 <template>
-  <div class="content">
-    <div class="loginBox">
-      <h2>云仓</h2>
-      <form action="">
-        <div class="item">
-          <input type="text" required />
-          <label for="">账号</label>
+  <div id="login">
+    <div class="top">
+      <span class="icon-close" @click="$router.go(-1)"></span>
+    </div>
+    <div class="login-container">
+      <div class="login-header logo">
+        <span class="icon icon-film-logo"></span>
+        <p class="title">淘气电影</p>
+        <i class="eng-title">Monkey Movie</i>
+      </div>
+      <div class="login-content">
+        <div class="login-type">
+          <span :class="{ active: loginType }" @click="changeLoginType"
+            >验证码登录</span
+          >
+          <span :class="{ active: !loginType }" @click="changeLoginType"
+            >密码登录</span
+          >
         </div>
-        <div class="item">
-          <input type="password" required />
-          <label for="">密码</label>
+        <div class="login-main">
+          <form>
+            <!--手机验证码登录部分-->
+            <div v-show="loginType" class="login-phone">
+              <section class="login-info">
+                <input type="tel" placeholder="手机号" v-model="phone" />
+                <button
+                  v-if="countDown === 0"
+                  :class="{ phone_right: checkPhone }"
+                  @click.prevent="getValidateCode"
+                >
+                  获取验证码
+                </button>
+                <button v-else class="phone_right">
+                  重新获取({{ countDown }}s)
+                </button>
+              </section>
+              <section class="login-info login-verification">
+                <input type="tel" placeholder="验证码" v-model="phoneCode" />
+              </section>
+              <section class="login-info login-hint">
+                温馨提示：未注册的手机号，验证后将自动注册账户，且代表已同意
+                <a href="javascript:;">《服务协议与隐私政策》</a>
+              </section>
+            </div>
+            <!--账号登录部分-->
+            <div v-show="!loginType" class="login-password">
+              <section class="login-info">
+                <input type="text" placeholder="用户名" v-model="userName" />
+              </section>
+              <section class="login-info login-verification">
+                <input
+                  type="password"
+                  placeholder="密码"
+                  v-show="!isShowPassword"
+                  v-model="password"
+                />
+                <input
+                  type="text"
+                  placeholder="密码"
+                  v-show="isShowPassword"
+                  v-model="password"
+                />
+                <div class="switch-show">
+                  <span
+                    class="icon-eye-close-fill"
+                    v-show="!isShowPassword"
+                    @click="changePwdShow"
+                  >
+                  </span>
+                  <span
+                    class="icon-eye-open-fill"
+                    v-show="isShowPassword"
+                    @click="changePwdShow"
+                  >
+                  </span>
+                </div>
+              </section>
+              <section class="login-info">
+                <input type="text" placeholder="验证码" v-model="captcha" />
+                <img
+                  class="captcha"
+                  :src="captchaSrc"
+                  alt="captcha"
+                  @click="refreshCaptcha"
+                />
+              </section>
+            </div>
+            <button class="login-submit" @click.prevent="login">登录</button>
+          </form>
         </div>
-        <button class="btn">
-          登录
-          <span></span>
-          <span></span>
-          <span></span>
-          <span></span>
-        </button>
-      </form>
+      </div>
     </div>
   </div>
 </template>
-<script setup></script>
+<script setup>
+import { ref } from "vue";
+import { Dialog } from "vant";
+// import { Toast } from "vant";
+const phone = ref(""); // 手机号码
+const phoneCode = ref(""); //手机验证码
+const userName = ref(""); //用户名
+const password = ref(""); //密码
+const captcha = ref(""); //图形验证码
+const loginType = ref(true); //登录方式
+const countDown = ref(0); //倒计时
+const isShowPassword = ref(false); //密码显示方式
+const captchaSrc = ref("http://localhost:3000/api/captcha");
 
-<style scoped>
-* {
-  margin: 0;
-  padding: 0;
-}
+//选择登录类型
+const changeLoginType = () => {
+  loginType.value = !loginType.value;
+};
+//选择密码显示方式
+const changePwdShow = () => {
+  isShowPassword.value = !isShowPassword.value;
+};
+// const checkPhone = computed(() => {
+//   return /^1[3|4|5|6|7|8][0-9]{9}$/.test(this.phone);
+// });
+// //检查手机格式
+//         checkPhone(){
+//           return /^1[3|4|5|6|7|8][0-9]{9}$/.test(this.phone);
+//         }
+// 账号密码登录
+//登录
+const login = async () => {
+  //判断登录模式
+  if (loginType.value) {
+    //手机登录
+    if (phone.value === "") {
+      Dialog.alert({
+        title: "提示",
+        message: "请输入手机号码",
+      });
+    }
+    // else if (!checkPhone.value) {
+    //   // MessageBox.alert("请输入正确的手机号码");
+    //  Dialog.alert({
+    //   title: "提示",
+    //   message: "请输入正确的手机号码",
+    // });
+    // }
+    else if (phoneCode.value === "") {
+      // MessageBox.alert("请输入手机验证码");
+      Dialog.alert({
+        title: "提示",
+        message: "请输入手机验证码",
+      });
+    } else {
+      // let json = await phoneLogin(this.phone, this.phoneCode);
+      // if (json.success_code === 200) {
+      //   Toast({
+      //     message: "登录成功",
+      //     position: "middle",
+      //     duration: 2000,
+      //   });
+      //   this.$router.go(-1);
+      // } else {
+      // MessageBox.alert(json.message);
+      Dialog.alert({
+        title: "提示",
+        message: "请输入手机号码",
+      });
+      // }
+    }
+  } else {
+    //账号密码登录
+    if (userName.value === "") {
+      // MessageBox.alert("请输入用户名");
+      Dialog.alert({
+        title: "提示",
+        message: "请输入用户名",
+      });
+    } else if (password.value === "") {
+      // MessageBox.alert("请输入密码");
+      Dialog.alert({
+        title: "提示",
+        message: "请输入密码",
+      });
+    } else if (captcha.value === "") {
+      // MessageBox.alert("请输入验证码");
+      Dialog.alert({
+        title: "提示",
+        message: "请输入验证码",
+      });
+    } else {
+      // let json = await pwdLogin(this.userName, this.password, this.captcha);
+      // if (json.success_code === 200) {
+      //   Toast({
+      //     message: "登录成功",
+      //     position: "middle",
+      //     duration: 2000,
+      //   });
+      //   this.$router.go(-1);
+      // } else {
+      // MessageBox.alert(json.message);
+      Dialog.alert({
+        title: "提示",
+        message: "请输入手机号码",
+      });
+      // this.refreshCaptcha();
+      // }
+    }
+  }
+};
+</script>
 
-a {
-  text-decoration: none;
-}
-
-input,
-button {
-  background: transparent;
-  border: 0;
-  outline: none;
-}
-
-.content {
-  height: 100vh;
-  background: linear-gradient(#141e30, #243b55);
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  font-size: 16px;
-  color: #03e9f4;
-}
-
-.loginBox {
-  width: 400px;
-  height: 364px;
-  background-color: #0c1622;
-  margin: 100px auto;
-  border-radius: 10px;
-  box-shadow: 0 15px 25px 0 rgba(0, 0, 0, 0.6);
-  padding: 40px;
-  box-sizing: border-box;
-}
-
-h2 {
-  text-align: center;
-  color: aliceblue;
-  margin-bottom: 30px;
-  font-family: "Courier New", Courier, monospace;
-}
-
-.item {
-  height: 45px;
-  border-bottom: 1px solid #fff;
-  margin-bottom: 40px;
-  position: relative;
-}
-
-.item input {
+<style lang="scss" scoped>
+#login {
   width: 100%;
   height: 100%;
-  color: #fff;
-  padding-top: 20px;
-  box-sizing: border-box;
-}
-
-.item input:focus + label,
-.item input:valid + label {
-  top: 0px;
-  font-size: 2px;
-}
-
-.item label {
-  position: absolute;
-  left: 0;
-  top: 12px;
-  transition: all 0.5s linear;
-}
-
-.btn {
-  padding: 10px 20px;
-  margin-top: 30px;
-  color: #03e9f4;
-  position: relative;
-  overflow: hidden;
-  text-transform: uppercase;
-  letter-spacing: 2px;
-  left: 35%;
-}
-
-.btn:hover {
-  border-radius: 5px;
-  color: #fff;
-  background: #03e9f4;
-  box-shadow: 0 0 5px 0 #03e9f4, 0 0 25px 0 #03e9f4, 0 0 50px 0 #03e9f4,
-    0 0 100px 0 #03e9f4;
-  transition: all 1s linear;
-}
-
-.btn > span {
-  position: absolute;
-}
-
-.btn > span:nth-child(1) {
-  width: 100%;
-  height: 2px;
-  background: -webkit-linear-gradient(left, transparent, #03e9f4);
-  left: -100%;
-  top: 0px;
-  animation: line1 1s linear infinite;
-}
-
-@keyframes line1 {
-  50%,
-  100% {
-    left: 100%;
+  color: #000;
+  font-size: 0.3125rem;
+  .top {
+    width: 100%;
+    height: 1rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: fixed;
+    top: 0;
+    left: 0;
+    color: #dd2727;
+    .icon-close {
+      font-size: 0.6rem;
+      position: absolute;
+      left: 0.3rem;
+    }
+    .name {
+      width: 60%;
+    }
   }
-}
-
-.btn > span:nth-child(2) {
-  width: 2px;
-  height: 100%;
-  background: -webkit-linear-gradient(top, transparent, #03e9f4);
-  right: 0px;
-  top: -100%;
-  animation: line2 1s 0.25s linear infinite;
-}
-
-@keyframes line2 {
-  50%,
-  100% {
-    top: 100%;
-  }
-}
-
-.btn > span:nth-child(3) {
-  width: 100%;
-  height: 2px;
-  background: -webkit-linear-gradient(left, #03e9f4, transparent);
-  left: 100%;
-  bottom: 0px;
-  animation: line3 1s 0.75s linear infinite;
-}
-
-@keyframes line3 {
-  50%,
-  100% {
-    left: -100%;
-  }
-}
-
-.btn > span:nth-child(4) {
-  width: 2px;
-  height: 100%;
-  background: -webkit-linear-gradient(top, transparent, #03e9f4);
-  left: 0px;
-  top: 100%;
-  animation: line4 1s 1s linear infinite;
-}
-
-@keyframes line4 {
-  50%,
-  100% {
-    top: -100%;
+  .login-container {
+    margin-top: 1.2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-flow: column;
+    .login-header {
+      display: flex;
+      flex-flow: column;
+      justify-content: center;
+      align-items: center;
+      margin-bottom: 0.2rem;
+      .icon {
+        font-size: 2rem;
+      }
+      .title,
+      .eng-title {
+        font-size: 0.3125rem;
+        font-weight: bolder;
+        text-align: center;
+        color: #dd2727;
+      }
+      .title {
+        text-indent: 0.2rem;
+        letter-spacing: 0.14rem;
+        margin-bottom: 0.04rem;
+      }
+      .eng-title {
+        letter-spacing: 0.02rem;
+        font-size: 0.21rem;
+      }
+    }
+    .login-content {
+      width: 100%;
+      background-color: #fff;
+      .login-type {
+        width: 100%;
+        font-size: 0.25rem;
+        font-weight: 700;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 0.25rem;
+        span {
+          margin: 0.25rem;
+          padding-bottom: 0.25rem;
+          box-sizing: border-box;
+        }
+        .active {
+          color: #dd2727;
+          border-bottom: 0.04rem solid #dd2727;
+        }
+      }
+      .login-main {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        form {
+          width: 100%;
+          .login-phone,
+          .login-password {
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-flow: column;
+            .login-info {
+              position: relative;
+              margin-bottom: 0.25rem;
+              width: 70%;
+              input {
+                width: 100%;
+                height: 0.8rem;
+                padding-left: 0.2rem;
+                box-sizing: border-box;
+                border: 0.02rem solid #888;
+                border-radius: 0.12rem;
+                outline: 0;
+                &:focus {
+                  border: 0.02rem solid #dd2727;
+                }
+              }
+            }
+            .login-info > button {
+              position: absolute;
+              top: 50%;
+              right: 0.2rem;
+              height: 80%;
+              transform: translateY(-50%);
+              border: 0;
+              color: #ccc;
+              font-size: 0.25rem;
+              background: transparent;
+              outline: none;
+              &.phone_right {
+                color: #e02e24;
+              }
+            }
+            .login-info > .switch-show,
+            .login-info > .captcha {
+              position: absolute;
+              top: 50%;
+              right: 0.2rem;
+              transform: translateY(-50%);
+            }
+            .login-info > .captcha {
+              height: 68%;
+            }
+            .login-hint {
+              color: #888;
+              font-size: 0.24rem;
+              line-height: 20px;
+              > a {
+                color: #dd2727;
+              }
+            }
+          }
+          .login-submit {
+            display: block;
+            width: 70%;
+            height: 0.8rem;
+            margin: 0.16rem auto;
+            border-radius: 0.12rem;
+            background: #dd2727;
+            color: #fff;
+            text-align: center;
+            font-size: 0.3125rem;
+            line-height: 0.8rem;
+            border: 0;
+          }
+        }
+      }
+    }
   }
 }
 </style>
