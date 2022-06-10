4f8b58716225fb17dfaaf78184f26256a0b5fd7a
zjp693 <942680978@qq.com>
2022-06-10 09:39:38 +0800

    update

--- phone/src/api/order.js
+++ phone/src/api/order.js
@@ -1,12 +1,17 @@
 import { requestWithToken } from "@/utils/request";
 
 /**
- *  用户下单
- * @param username 账号
- * @param password 密码
+ * 用户下单
+ * @param userId  用户id
+ * @param scheduleId 电影id
+ * @param orderPhone 订单id
+ * @param orderDate 订单信息
+ * @param ticketNum 买的票数
+ * @param totalPrice 总价
+ * @param orderSeatInfo
+ * @param payType 支付状态
  * @returns {AxiosPromise}
  */
-
 export function getUserOrder(
   userId,
   scheduleId,
